1aae5f6bdc5908bb26a62cbc28cba3495b566be4
ShojxJc <Cajustejoshua@gmail.com>
2025-04-14 18:40:02 -0400

    copy and paste

--- hellotest.m
+++ hellotest.m
@@ -16,20 +16,21 @@ main(!IO) :-
     io.write_string("Lets go Gambling!!!!!!! \n", !IO),
     io.write_string("Call me a good boy first though \n", !IO),
     io.write_string("\n", !IO),
+
     Lookforthis = "Good Boy",
     io.read_line_as_string(User, !IO),
     (
         User = ok(Line),
         Input = string.strip(Line),
         (
-        Input = Lookforthis
-        ->
-            io.write_string("Omg im gonna ferk \n", !IO)
-             random.int(State, 10, ProgNum, NewState), % Generate a random number
-            play_guessing_game(ProgNum, NewState, !IO).
-        ;
-            io.write_string("Meanie \n", !IO),
-            io.set_exit_status(1, !IO)
+            Input = Lookforthis
+            -> 
+                io.write_string("Omg I'm gonna ferk \n", !IO),
+                random.int(State, 10, ProgNum, NewState), % Generate a random number
+                play_guessing_game(ProgNum, NewState, !IO)
+            ;
+                io.write_string("Meanie \n", !IO),
+                io.set_exit_status(1, !IO)
         )
     ;
         User = eof,
@@ -40,12 +41,8 @@ main(!IO) :-
         io.write_string("oopsie \n", !IO),
         io.set_exit_status(1, !IO)
     ).
-    
-
-
 
 :- pred loop(int::in, int::in, io::di, io::uo) is det.
-
 loop(N, Max, !IO) :-
     ( if N =< Max then
         io.write_string("\n", !IO),
@@ -56,19 +53,18 @@ loop(N, Max, !IO) :-
 
 :- pred play_guessing_game(int::in, random.state::in, io::di, io::uo) is det.
 play_guessing_game(ProgNum, State, !IO) :-
-io.write_string("\n Lets play guess the number \n Im thinking of number between 1 - 10", !IO),
+    io.write_string("\nLets play guess the number \nI'm thinking of a number between 1 - 10", !IO),
     io.read_line_as_string(UNum, !IO),
     (
         UNum = ok(Sinep),
         Vinny = string.strip(Sinep),
         string.to_int(Vinny, GAM),
         (
-        GAM = ProgNum
-        ->
-            io.write_string("Holy shit you got it write\n", !IO)
-        ;
-            io.write_string("Lmao Type shit \n", !IO),
-            io.set_exit_status(1, !IO)
+            GAM = ProgNum
+            -> io.write_string("Holy shit you got it right\n", !IO)
+            ;
+                io.write_string("Lmao, incorrect. Try again! \n", !IO),
+                io.set_exit_status(1, !IO)
         )
     ;
         UNum = eof,
@@ -76,6 +72,6 @@ io.write_string("\n Lets play guess the number \n Im thinking of number between 
         io.set_exit_status(1, !IO)
     ;
         UNum = error(_),
-        io.write_string("oopsie \n", !IO),
+        io.write_string("Oopsie \n", !IO),
         io.set_exit_status(1, !IO)
     ).

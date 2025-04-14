2110e6615cf6f9620bfea6f73a7b499859ded78c
ShojxJc <Cajustejoshua@gmail.com>
2025-04-14 17:10:51 -0400

    this is stupid

--- hellotest.m
+++ hellotest.m
@@ -8,5 +8,5 @@
 :- import_module string.
 
 main(!IO) :-
-    s1 = "ferker i hardly know her"
-    io.write_string(s1 ++ "/n", !IO).
+    S1 = "ferker i hardly know her"
+    io.write_string(S1 ++ "\n", !IO)

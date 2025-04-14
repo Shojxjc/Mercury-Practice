bbe6cba92d46d4c5edd7f5785ce8c22e21d173b2
ShojxJc <Cajustejoshua@gmail.com>
2025-04-14 17:07:40 -0400

    this is stupid

--- hellotest.m
+++ hellotest.m
@@ -5,6 +5,8 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
+:- import_module string.
 
 main(!IO) :-
-    io.write_string("Hello, World!\n", !IO).
+    s1 = "ferker i hardly know her"
+    io.write_string(s1"/n", !IO).

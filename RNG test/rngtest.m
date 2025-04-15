8b5d06ac33848cf1d5f818c4cbc1b1b637f07cdc
ShojxJc <Cajustejoshua@gmail.com>
2025-04-15 13:22:53 -0400

    Rand test

--- RNG test/rngtest.m
+++ RNG test/rngtest.m
@@ -10,10 +10,8 @@
 
 :- pred uniform_int_in_range(int::in, int::in, int::out, R::in, R::out)
     is det <= random(R).
-
-% Get the current time and use it as a seed
-    time.current_time(Time),
-    Seed = time.to_int(Time),
+    
+    Seed = 12345,
 
     % Use sfc32 random number generator and initialize it with the seed
     sfc32.init(Seed, R0),

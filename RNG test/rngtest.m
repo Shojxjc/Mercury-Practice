5509d46563ed52289cbd21b4845feb25f1dc85e6
ShojxJc <Cajustejoshua@gmail.com>
2025-04-15 13:46:06 -0400

    Rand test

--- RNG test/rngtest.m
+++ RNG test/rngtest.m
@@ -8,14 +8,13 @@
 :- import_module int, time, string.
 :- import_module list.
 :- import_module random.
-:- import_module random.sfc32.
 
 main(!IO) :-
     SeedInt = 12345,
-    sfc32.init(int.to_uint(SeedInt), R0),
+    init(int.to_uint(SeedInt), R0),
 
     % Generate a random number in the range 1 to 10
-    sfc32.uniform_int_in_range(1, 10, Number, R0, _),
+    uniform_int_in_range(1, 10, Number, R0, _),
 
     % Print the random number
     io.format("Your random number is: %d\n", [i(Number)], !IO).

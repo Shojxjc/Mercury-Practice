92c3286b5c4043fad235f36a7cd1b08eb826ac94
ShojxJc <Cajustejoshua@gmail.com>
2025-04-15 13:41:59 -0400

    Rand test

--- RNG test/rngtest.m
+++ RNG test/rngtest.m
@@ -12,9 +12,7 @@
 
 main(!IO) :-
     SeedInt = 12345,
-    sfc32.make_params_from_int(SeedInt, Params),
-    % Use sfc32 random number generator and initialize it with the seed
-    sfc32.init(Params, R0),
+    sfc32.init(SeedInt, R0),
 
     % Generate a random number in the range 1 to 10
     uniform_int_in_range(1, 10, Number, R0, _),

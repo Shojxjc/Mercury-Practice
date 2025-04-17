bfd40af70b5b4dc697812e5ecee0af395dd01573
ShojxJc <Cajustejoshua@gmail.com>
2025-04-17 11:15:53 -0400

    messgae

--- Final Program/pres.m
+++ Final Program/pres.m
@@ -27,28 +27,30 @@ main(!IO) :-
    (
       UNum = ok(Sinep),
       Vinny = string.strip(Sinep),
-      (if string.to_int(Vinny) then
+      (if string.to_int(Vinny,NumFish) then
       (
-         loop(1,Vinny, !IO)
+         loop(1,NumFish, !IO)
       )
       else
          io.write_string("That's not even a number  \n", !IO),
          io.set_exit_status(1, !IO)
       )
-      ;
+   ;
       UNum = eof,
          io.write_string("What are you ignoring me \n", !IO),
          io.set_exit_status(1, !IO)
-      ;
+   ;
       UNum = error(_),
          io.write_string("You must love kanye \n", !IO),
          io.set_exit_status(1, !IO)
-   )
+   ),
    io.write_string("I Caught the fish now call me a Good Boy !!\n", !IO),
    io.read_line_as_string(User, !IO),
    (
       User = ok(Line),
       Input = string.strip(Line),
+      Lookforthis = "Good boy"
+      Semiok = "Good Program"
       (
          Input = Lookforthis
          -> 

7ff1481da3ea1697e7f756f82e5b9d546c97a4d0
ShojxJc <Cajustejoshua@gmail.com>
2025-04-17 11:36:54 -0400

    alot of commits

--- Final Program/pres.m
+++ Final Program/pres.m
@@ -49,8 +49,8 @@ main(!IO) :-
    (
       User = ok(Line),
       Input = string.to_lower(string.strip(Line)),
-      Lookforthis = "Good boy",
-      Semiok = "Good Program",
+      Lookforthis = "good boy",
+      Semiok = "good Program",
       ( if Input = Lookforthis then
             io.write_string("Omg I'm gonna ferk 💦\n", !IO)
          else if Input = Semiok then
